2d3895daee45e3d007282c86c5118d32064957b7
moapochta06 <90915948+moapochta06@users.noreply.github.com>
2024-11-11 23:40:00 +0700

    localhost:8089

--- planit/utilities.py
+++ planit/utilities.py
@@ -9,7 +9,7 @@ def send_activation_notification(user):
    if ALLOWED_HOSTS:
        host = 'http://' + ALLOWED_HOSTS[0]
    else:
-       host = 'http://localhost:8000'
+       host = 'http://localhost:8089'
    context = {'user': user, 'host': host, 'sign': signer.sign(user.username)}
    subject = render_to_string('email/activation_letter_subject.txt',
                               context)
